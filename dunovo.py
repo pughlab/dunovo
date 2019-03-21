41106fa412ad4bbe58089c3d280cfbba50ad3562
Nick <nmapsy@gmail.com>
2019-03-20 23:43:02 -0700

    dunovo.py: Add some output validation (currently just for baralign.sh).

--- dunovo.py
+++ dunovo.py
@@ -12,7 +12,7 @@ DESCRIPTION = """Run the entire Du Novo pipeline."""
 
 
 def make_argparser():
-  parser = argparse.ArgumentParser(description=DESCRIPTION)
+  parser = argparse.ArgumentParser(description=DESCRIPTION, add_help=False)
   io = parser.add_argument_group('Inputs and outputs')
   io.add_argument('fastq1', metavar='reads_1.fq', type=open_as_text_or_gzip,
     help='Input reads (mate 1). Can be gzipped.')
@@ -67,6 +67,11 @@ def make_argparser():
     default=logging.WARNING)
   volume.add_argument('-v', '--verbose', dest='volume', action='store_const', const=logging.INFO)
   volume.add_argument('--debug', dest='volume', action='store_const', const=logging.DEBUG)
+  misc = parser.add_argument_group('Miscellaneous')
+  misc.add_argument('-V', '--no-validate', dest='validate', action='store_false', default=True,
+    help='Skip validation checks on the outputs of individual scripts.')
+  misc.add_argument('-h', '--help', action='help',
+    help='Print this help text and exit.')
   return parser
 
 
@@ -114,6 +119,9 @@ def main(argv):
   ]
   run_pipeline(steps)
 
+  if args.validate:
+    if not validate_baralign_output(paths['families'], paths['refdir'], paths['correct_sam']):
+      fail('Error: baralign.sh output not as expected.')
 
   # The 3rd pipeline.
   steps = [
@@ -267,17 +275,17 @@ def make_paths(outdir_arg, log_dir, fastq1_path, suffix_arg):
 
 
 def invalid_paths(paths, log_paths, log_dir):
-  invalid_paths = False
+  paths_are_invalid = False
   for path in list(paths.values()) + list(log_paths.values()):
     if path is None:
       continue
     if not os.path.isdir(os.path.dirname(path)):
       logging.critical('Error: Directory {!r} doesn\'t exist.'.format(os.path.dirname(path)))
-      invalid_paths = True
+      paths_are_invalid = True
     if os.path.exists(path):
       logging.critical('Error: {!r} already exists.'.format(path))
-      invalid_paths = True
-  return invalid_paths
+      paths_are_invalid = True
+  return paths_are_invalid
 
 
 def open_logs(log_paths):
@@ -365,7 +373,55 @@ def get_generic_args(arg_list, flag_list, kwargs):
   return args
 
 
-def run_pipeline(steps, stdin=None, stdout=None):
+def validate_baralign_output(families_path, refdir_path, correct_sam_path):
+  is_valid = True
+  steps = [
+    {'command':['cut', '-f', '1', families_path]},
+    {'command':['tail', '-n', '1']}
+  ]
+  last_barcode = run_pipeline(steps, stdout=subprocess.PIPE)
+  result1 = subprocess.call(['grep', '-q', last_barcode, os.path.join(refdir_path, 'barcodes.fa')])
+  result2 = subprocess.call(['grep', '-q', last_barcode, os.path.join(refdir_path, 'barcodes-ref.fa')])
+  if not (result1 == 0 and result2 == 0):
+    logging.error('Error: Last barcode {} missing from {}.'.format(last_barcode, refdir_path))
+    is_valid = False
+  steps = [
+    {'command':['grep', '-B', '1', last_barcode, os.path.join(refdir_path, 'barcodes.fa')]},
+    {'command':['head', '-n', '1']},
+    {'command':['tail', '-c', '+2']},
+  ]
+  last_barname = run_pipeline(steps, stdout=subprocess.PIPE)
+  steps = [
+    {'command':['awk', '-F', '\\t', '$1 == {}'.format(last_barname), correct_sam_path]},
+    {'command':['wc', '-l']}
+  ]
+  alignments = run_pipeline(steps, stdout=subprocess.PIPE, stdout_type=int)
+  if alignments == 0:
+    logging.error('Error: Last family {} doesn\'t appear in the {} alignment.'
+                  .format(last_barcode, correct_sam_path))
+    is_valid = False
+  logging.info('Info: baralign.sh seems to have worked!')
+  return is_valid
+
+
+def run_pipeline(steps, stdin=None, stdout=None, stdout_type=str):
+  """Run command pipeline defined in the list `steps`.
+  `stdin`:  Where the first command should get its stdin.
+            Can be a string (the path to a file), an open file, or a dict. A dict indicates the
+            stdin should be the output of a Python function. The key `function` in the dict
+            should be the function (as a Python object), and the key `fxn_args` should be the
+            list of arguments to give to the function.
+  `stdout`: Where the last command should put its stdout. Can be a string (the path to a file), an
+            open file, or subprocess.PIPE. In the latter case, this means to return the stdout from
+            this function (converted to a utf-8 string).
+  `stdout_type`: When `stdout` is subprocess.PIPE, use this function to convert the raw stdout bytes
+                 before returning it. If the value is `str` (the default), it will be converted to
+                 a utf-8 string and any trailing newline will be trimmed.
+  Keys for each step:
+    `command`: The command to execute. Should be a list. Passed directly to subprocess.Popen().
+    `stderr`:  Where to put the stderr of the command. Should be either an open file, sys.stderr, or
+               subprocess.DEVNULL.
+  """
   processes = []
   fxn_stdin = False
   for i, step in enumerate(steps):
@@ -373,9 +429,11 @@ def run_pipeline(steps, stdin=None, stdout=None):
     kwargs = {}
     # Determine the inputs and outputs.
     #   stderr
-    if step['stderr']:
+    if step.get('stderr'):
       kwargs['stderr'] = step['stderr']
-      if step['stderr'] is not sys.stderr:
+      if step['stderr'] == subprocess.DEVNULL:
+        cmd_str += ' 2> {}'.format(os.devnull)
+      elif step['stderr'] is not sys.stderr:
         cmd_str += ' 2> '+step['stderr'].name
     #  stdin
     if i == 0:
@@ -396,21 +454,26 @@ def run_pipeline(steps, stdin=None, stdout=None):
       kwargs['stdin'] = processes[i-1].stdout
       cmd_str = '  | '+cmd_str
     #   stdout
-    if i == len(steps)-1:
-      if isinstance(stdout, str):
+    if i < len(steps)-1:
+      # We're in an intermediate step.
+      kwargs['stdout'] = subprocess.PIPE
+      cmd_str += ' \\'
+    else:
+      # We're on the last step.
+      if stdout == subprocess.PIPE:
+        kwargs['stdout'] = subprocess.PIPE
+      elif isinstance(stdout, str):
         kwargs['stdout'] = open(stdout, 'w')
         cmd_str += ' > '+stdout
       else:
         kwargs['stdout'] = stdout
         if hasattr(stdout, 'name'):
           cmd_str += ' > '+stdout.name
-    else:
-      kwargs['stdout'] = subprocess.PIPE
-      cmd_str += ' \\'
     # Create the actual process and add it to the list.
     processes.append(subprocess.Popen(step['command'], **kwargs))
     logging.warning(cmd_str)
-  # Kick it off by closing the first stdout (don't ask me).
+  # Kick it off by closing the first stdout. The docs say this is necessary in order for the first
+  # process to receive a SIGPIPE if the second (or later?) process exits before the first process.
   if processes[0].stdout:
     processes[0].stdout.close()
   # If the input is from a function, start feeding it in.
@@ -421,10 +484,24 @@ def run_pipeline(steps, stdin=None, stdout=None):
       processes[0].stdin.write(line)
     processes[0].stdin.close()
   # Wait for all processes to finish and check their exit codes.
-  for process in processes:
-    result = process.wait()
-    if result != 0:
-      fail('Error: Process exited with code {}: $ {}'.format(result, ' '.join(process.args)))
+  for i, process in enumerate(processes):
+    if i < len(steps)-1 or stdout != subprocess.PIPE:
+      result = process.wait()
+      if result != 0:
+        fail('Error: Process exited with code {}: $ {}'.format(result, ' '.join(process.args)))
+    else:
+      # Last step, whose output has been requested as the return value.
+      stdout_bytes, stderr_bytes = process.communicate()
+      if process.returncode != 0:
+        fail('Error: Process exited with code {}: $ {}'.format(result, ' '.join(process.args)))
+      try:
+        if stdout_type is str:
+          return str(stdout_bytes, 'utf8').rstrip('\r\n')
+        else:
+          return stdout_type(stdout_bytes)
+      except ValueError as error:
+        fail('Error: Encountered {} when converting output of command to {}: $ {}'
+             .format(type(error).__name__, stdout_type.__name__, ' '.join(process.args)))
 
 
 def fail(message):
