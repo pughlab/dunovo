7cf814fd207f308fd29e5e4f15c7d7b0f1323d04
Nick <nmapsy@gmail.com>
2020-10-28 14:20:06 -0400

    tests/unit-tests.py: Switch to Python 3 only.

--- tests/unit-tests.py
+++ tests/unit-tests.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import absolute_import
+#!/usr/bin/env python3
 import argparse
 import logging
 import os
